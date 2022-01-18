7c5363916d869d432e4fab7e6bd35f438bc446c8
Jan Möbius <moebius@cs.rwth-aachen.de>
2022-01-18 09:50:04 +0100

    Updated license header

--- src/OpenMesh/Core/IO/MeshIO.hh
+++ src/OpenMesh/Core/IO/MeshIO.hh
@@ -1,7 +1,7 @@
 /* ========================================================================= *
  *                                                                           *
  *                               OpenMesh                                    *
- *           Copyright (c) 2001-2015, RWTH-Aachen University                 *
+ *           Copyright (c) 2001-2022, RWTH-Aachen University                 *
  *           Department of Computer Graphics and Multimedia                  *
  *                          All rights reserved.                             *
  *                            www.openmesh.org                               *
