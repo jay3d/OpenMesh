99db404cc67096d1327d3d685b7731f14ed9e316
Jan Möbius <moebius@cs.rwth-aachen.de>
2023-03-01 15:24:30 +0100

    Reduce allowed cppcheck warnings

--- CI/ci-cppcheck.sh
+++ CI/ci-cppcheck.sh
@@ -32,7 +32,7 @@ echo "CPPCHECK Summary"
 echo "=============================================================================="
 echo -e "${NC}"
 
-MAX_COUNT=30
+MAX_COUNT=24
 
 if [ $COUNT -gt $MAX_COUNT ]; then
   echo -e ${WARNING}
